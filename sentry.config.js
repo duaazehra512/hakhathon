04bf2795f88df2d19d02b1b05d29e28fa4094fe7
duaazehra512 <165053825+duaazehra512@users.noreply.github.com>
2025-02-08 23:01:34 +0500

    Add files via upload

---
+++ sentry.config.js
@@ -0,0 +1,8 @@
+const {withSentryConfig} = require("@sentry/nextjs");
+const moduleExports = {
+
+};
+const SentryWebpackPluginOptions ={
+    silent: true,
+};
+module.exports=withSentryConfig(moduleExports,SentryWebpackPluginOptions);
